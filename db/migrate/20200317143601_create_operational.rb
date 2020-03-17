8a180c83b4553199f4f9ecdacfef0102c8f220ee
kevinrizkhy <kevin.rizkhy85@gmail.com>
2020-03-17 21:38:47 +0700

    Migration operational

---
+++ db/migrate/20200317143601_create_operational.rb
@@ -0,0 +1,12 @@
+class CreateOperational < ActiveRecord::Migration[5.2]
+  def change
+    create_table :operationals do |t|
+    	t.timestamp :date, null: false
+    	t.references :store, foreign_key: true, null: false
+    	t.references :user, foreign_key: true, null: false
+    	t.bigint :nominal, null: false
+
+    	t.timestamps
+    end
+  end
+end
